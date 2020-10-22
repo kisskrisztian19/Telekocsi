2421e6b4a9d8d5b0415c9bb01fb14ed291eaca22
krisztián kiss <kisskrisztiannnnn@hotmail.com>
2020-10-22 09:27:42 +0200

    Osztályok beállítása

---
+++ TelekocsiForm/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelekocsiForm
+{
+    class Auto
+    {
+        public string Indulas { get; private set; }
+        public string Cel { get; private set; }
+        public string Rendszam { get; private set; }
+        public string Telszam { get; private set; }
+        public int Ferohely { get; private set; }
+        public string Utvonal { get; private set; }
+        public Hirdetok(string indulas, string cel, string rendszam, string telszam, int ferohely)
+        {
+            Indulas = indulas;
+            Cel = cel;
+            Rendszam = rendszam;
+            Telszam = telszam;
+            Ferohely = ferohely;
+            Utvonal = Indulas + "-" + Cel;
+        }
+}
---
+++ TelekocsiForm/Igeny.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelekocsiForm
+{
+    class Igeny
+    {
+
+        public string Azon { get; private set; }
+        public string Indulas { get; private set; }
+        public string Cel { get; private set; }
+        public int Emberek { get; private set; }
+        public string Utvonal { get; private set; }
+        public Igeny(string azon, string indulas, string cel, int emberek)
+        {
+            Azon = azon;
+            Indulas = indulas;
+            Cel = cel;
+            Emberek = emberek;
+            Utvonal = Indulas + "-" + Cel;
+        }
+
+        public int VanAuto(List<Auto> autok)
+        {
+            int i = 0;
+            while (i < autok.Count && !(Utvonal == autok[i].Utvonal && Emberek <= autok[i].Ferohely))
+            {
+                i++;
+            }
+
+            if (i < autok.Count)
+            {
+                return i;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+}
